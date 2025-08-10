ec3735459bd37bb4f020492029c2254752e4bc34
Chris Armitage <carmitage.5@gmail.com>
2025-08-10 12:19:54 -0400

    asdf

--- client/src/components/coming-soon/ComingSoonPopup.jsx
+++ client/src/components/coming-soon/ComingSoonPopup.jsx
@@ -7,7 +7,7 @@ const ComingSoonPopup = ({ onClose }) => (
       <h3>Coming Soon</h3>
       <p>
         Facebook, and Instagram pages are not currently available (coming soon).<br />
-        If you find Aiden's rock in the meantime, please either use the website to upload the pictures or email us!<br />
+        If you find Aiden's rock in the meantime, please either use the website to upload the pictures or email us! (AidensRocks.AAA@gmail.com)<br />
         Thank you.
       </p>
       <button onClick={onClose}>Close</button>
